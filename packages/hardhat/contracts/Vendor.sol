4af89848183da863ac2531913de49af9c4a4b174
RAHUL DINDIGALA <rahul.dindigala32@outlook.com>
2025-12-09 15:29:49 +0530

    feat: write the vendor contract logic to buy & transfer ERC20 tokens and test the functionality on local chain

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,21 +1,38 @@
 pragma solidity 0.8.20; //Do not change the solidity version as it negatively impacts submission grading
 // SPDX-License-Identifier: MIT
 
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
-contract Vendor {
-    // event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
-
+contract Vendor is Ownable{
+    /* State variables */
     YourToken public yourToken;
+    uint256 public constant tokensPerEth = 100;
 
-    constructor(address tokenAddress) {
+    /* Events */
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+
+    constructor(address tokenAddress) Ownable(msg.sender) {
         yourToken = YourToken(tokenAddress);
     }
 
     // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        require(msg.value > 0, "Send ETH to buysome tokens");
+        uint256 amountToBuy = msg.value * tokensPerEth;
+        uint256 vendorBalance = yourToken.balanceOf(address(this));
+        require(vendorBalance >= amountToBuy, "Vendor insufficient tokens");
+        bool success = yourToken.transfer(msg.sender, amountToBuy);
+        require(success, "Token transfer failed");
+        emit BuyTokens(msg.sender, msg.value, amountToBuy);
+    }
 
     // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public onlyOwner {
+        uint256 vendorEthBalance = address(this).balance;
+        (bool success, ) = msg.sender.call{value: vendorEthBalance}("");
+        require(success, "Withdraw failed");
+    }
 
     // ToDo: create a sellTokens(uint256 _amount) function:
 }
